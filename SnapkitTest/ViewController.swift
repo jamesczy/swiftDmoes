1bde3cc7d50767d5f8d08f245d54738f48a0a61f
jamesChen <james@macdeimac.local>
2018-12-25 10:03:00 +0800

    Initial Commit

---
+++ SnapkitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapkitTest
+//
+//  Created by jamesChen on 2018/12/25.
+//  Copyright © 2018年 jamesChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
